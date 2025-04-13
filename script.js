aac6dc4d5996276339f7cf40a21412e41f12b651
Md. Ehsanul Haque <159643324+EhsanulHaque75@users.noreply.github.com>
2025-04-13 20:10:54 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thanks for contacting me! I'll get back to you soon.");
+  });
+  
